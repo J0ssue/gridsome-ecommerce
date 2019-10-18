7dd63face8f06b429225578e831b0b2a651061f3
Josue Diaz <jdiaz77711@gmail.com>
2019-10-18 12:27:54 +0100

    rebuilt with node 10

--- src/store/index.js
+++ src/store/index.js
@@ -12,18 +12,18 @@ Vue.use(Vuex);
  * directly export the Store instantiation
  */
 
-export default function( /* { ssrContext } */ )
-const Store = new Vuex.Store({
-  modules: {
-    products,
-    cart,
-    purchases
-  },
+export default function( /* { ssrContext } */ ) {
+  const Store = new Vuex.Store({
+    modules: {
+      products,
+      cart,
+      purchases
+    },
 
-  // enable strict mode (adds overhead!)
-  // for dev mode only
-  // strict: process.env.DEV
-});
+    // enable strict mode (adds overhead!)
+    // for dev mode only
+    // strict: process.env.DEV
+  });
 
-return Store;
+  return Store;
 }
